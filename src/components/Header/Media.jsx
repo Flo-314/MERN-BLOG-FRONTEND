550115ac67011518a9c7cfb097be380f918760f9
Flo-314 <julianflo1916@gmail.com>
2022-01-21 20:20:07 -0300

    logos del header y footer puestos

--- src/components/Header/Media.jsx
+++ src/components/Header/Media.jsx
@@ -1,4 +1,7 @@
 import {Flex} from "@chakra-ui/react";
+import {LogoLinkedinIcon} from "chakra-ui-ionicons";
+import {LogoTwitterIcon} from "chakra-ui-ionicons";
+import {LogoFacebookIcon} from "chakra-ui-ionicons";
 
 function Media() {
   return (
@@ -7,17 +10,23 @@ function Media() {
         <Flex gap={5}>
           <li>
             <button>
-              <img alt="Linkedin" src="" />
+              <a href="https://www.linkedin.com/in/juli%C3%A1n-mat%C3%ADas-fl%C3%B3-931226222/">
+                <LogoLinkedinIcon color="primary.strong" h={8} w={8} />
+              </a>
             </button>
           </li>
           <li>
             <button>
-              <img alt="Facebook" src="" />
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=2s&ab_channel=RickAstley">
+                <LogoFacebookIcon color="primary.strong" h={8} w={8} />
+              </a>
             </button>
           </li>
           <li>
             <button>
-              <img alt="Twitter" src="" />
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=2s&ab_channel=RickAstley">
+                <LogoTwitterIcon color="primary.strong" h={8} w={8} />
+              </a>
             </button>
           </li>
         </Flex>
