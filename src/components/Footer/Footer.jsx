888de2f80588aa79077cfb25873476f29a50cfa5
Flo-314 <julianflo1916@gmail.com>
2022-01-21 16:32:23 -0300

    esqueleto del footer hecho

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,32 @@
+import {Box, Button, Flex, Heading, Image, Text} from "@chakra-ui/react";
+
 function Footer() {
-  return <div />;
+  return (
+    <footer>
+      <Flex direction={"column"}>
+        <Heading>FlóBlog</Heading>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+            <li>
+              <a href="/about">About Us</a>
+            </li>
+          </ul>
+        </nav>
+        <Box>
+          <Text>Blog create to complement the Rest api that i made.</Text>
+          <Text>
+            <a href="https://github.com/Flo-314"> My Github</a>
+          </Text>
+        </Box>
+      </Flex>
+    </footer>
+  );
 }
 
 export default Footer;
